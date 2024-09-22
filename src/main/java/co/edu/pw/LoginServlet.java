6aa773a3ff2496357de5b595a8844727973c01b0
Diego Carvajal <diego.carvajal@usco.edu.coclear>
2024-09-22 18:44:55 -0500

    Initial Commit

---
+++ src/main/java/co/edu/pw/LoginServlet.java
@@ -0,0 +1,60 @@
+package co.edu.pw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		request.getRequestDispatcher("link.html").include(request, response);
+
+		String name = request.getParameter("name");
+		String password = request.getParameter("password");
+
+		if (password.equals("admin123")) {
+			out.print("Welcome, " + name);
+			HttpSession session = request.getSession();
+			session.setAttribute("name", name);
+		} else {
+			out.print("Sorry, username or password error!");
+			request.getRequestDispatcher("login.html").include(request, response);
+		}
+		out.close();
+	}
+}
---
+++ src/main/java/co/edu/pw/LogoutServlet.java
@@ -0,0 +1,47 @@
+package co.edu.pw;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogoutServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		request.getRequestDispatcher("link.html").include(request, response);
+
+		HttpSession session = request.getSession();
+		session.invalidate();
+
+		out.print("You are successfully logged out!");
+
+		out.close();
+	}
+
+	
+}
